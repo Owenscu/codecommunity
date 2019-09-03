5674d3a25ea420dc633041bf87a60cbadb0edd07
Owenscu <1012064874@qq.com>
2019-09-03 21:00:45 +0800

    first

---
+++ src/main/java/com/ccnl/codecommunity/CodecommunityApplication.java
@@ -0,0 +1,13 @@
+package com.ccnl.codecommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodecommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodecommunityApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/ccnl/codecommunity/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.ccnl.codecommunity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    //contrl+p提示
+    public String hello(@RequestParam(name = "name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
